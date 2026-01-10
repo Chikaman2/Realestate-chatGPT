992e08d4ad9d2eaa46f36f7f85bd851048095d0b
Chika <chikaman3@gmail.com>
2026-01-10 09:22:02 +0100

    CORS update

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.config import Config
 from app.extensions import db, migrate, jwt
 from app.routes.auth import auth_bp
+from flask_cors import CORS
 from app.routes.property import property_bp
 
 from dotenv import load_dotenv
@@ -10,6 +11,14 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={
+        r"/api/*": {
+            "origins": [
+                "http://localhost:5173",
+                "      "
+            ]
+        }
+    })
     app.config.from_object(Config)
 
     db.init_app(app)
